93dde81c92056930388c5f095df9ed99f8d88017
riya <saxenaritu1979@gmail.com>
2020-03-20 15:43:05 +0530

    title of program and object creation of Scanner class

--- src/input/input.java
+++ src/input/input.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class input {
     public void userFunction() {
         try {
-
+            System.out.println("*****Tree Using Array*****");
+            Scanner sc = new Scanner(System.in);
         }
         catch(InputMismatchException e){
             //block to handle exception
