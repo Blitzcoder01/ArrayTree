34c435c1efc11407952fe298bb3e37211bdfd365
riya <saxenaritu1979@gmail.com>
2020-03-20 15:27:06 +0530

    declaring array of string

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,5 @@
 package main;
-
-import array_tree.ArrayTree;
 import input.input;
-
-import java.util.Scanner;
-
 public class main {
     public static void main(String[] args){
         input ob= new input();
